16df51f1787f8a3f11f65192d11d7bb1129e958b
Scola <scolagracemukamimurote@gmail.com>
2020-01-28 10:07:00 +0200

    Updaed Last Stage

--- src/App.js
+++ src/App.js
@@ -49,17 +49,19 @@ console.log("Recipes", recipes);
           type='submit'>Search
             </button>
       </form>
+      <div className = "recipes">
       {recipes.map(recipe => (
         <Recipe
           key={recipe.recipe.label}
           title={recipe.recipe.label}
           calories={recipe.recipe.calories}
           image={recipe.recipe.image}
-          ingredients={recipe.recipe.ingredients}
+          ingredients ={recipe.recipe.ingredients}
         />
       ))}
       <Recipe />
       {/* <Practice /> */}
+    </div>
     </div>
   );
 }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,29 +1,17 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
+import style from './recipe.module.css';
 
 const Recipe = ({ title, calories, image, ingredients }) => {
-   
-   // let ing = {ingredients}
-    //console.log("This is ", ingredients)
- 
-
-
-    return (
-        <div >
-            <h1>{title}</h1>
-                 {/* <ol>
-                    {ingredients.map((ingredient) => (
-                        <li> {ingredient.text}</li>
-                    ))}
-            </ol>      */}
-             <p>This recipe contains {calories} calories.</p>
-            <img src={image} alt="" /> 
-
-        </div>
-    )
-}
-
+  // let ing = {ingredients}
+  //console.log("This is ", ingredients)
+  return (
+    <div className =  {style.recipe}>
+      <h1>{title}</h1> 
+      <ol>{ingredients && ingredients.map(ingredient => <li> {ingredient.text}</li>)}</ol>
+      <p>This recipe contains {calories} calories.</p>
+      <img src={image} alt='' />
+    </div>
+  );
+};
 export default Recipe;
-
-
 // useState is a funcion that receives an array of 2 elements, the first being the value and the second one being a function to set that value
